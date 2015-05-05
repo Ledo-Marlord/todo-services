4811f217a2746cc5d134d564e4dc722f9c3d2a7b
Greg <harrison.greg.c@gmail.com>
2015-05-05 10:07:52 -0400

    Initi

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var db = require('./config/db');
+var apiRoutes = require('./app/routes/apiRoutes');
+var app = express();
+
+app.set('port', process.env.PORT || 1337);
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+app.use(bodyParser.json());
+
+// Add headers
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use('/api', apiRoutes);
+
+app.listen(app.get('port'));
+console.log('Listening on port: ' + app.get('port') + '...');
+console.log('\r\n');
