c0be7cf75e324db5415f7ce0d9c934e4d892903a
Scott Archer <LV-SARCHER@LV-SARCHER.local>
2015-05-06 14:23:17 -0400

    added gulp and set up gulp-nodemon with the project. The project could be just run with nodemon, but this is just in case gulp needs to be added later for more complex tasks, which is likely.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon');
+
+gulp.task('develop', function () {
+    nodemon({
+        script: 'server.js',
+        ext: 'js'
+    });
+});
+
+gulp.task('default', [
+    'develop'
+]);
